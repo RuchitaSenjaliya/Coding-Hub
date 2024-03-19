0de2aac7f9d765c6d758c885618e948378e0bbe7
Ruchita <senjaliyaruchita@gmail.com>
2024-03-19 12:30:31 +0530

    code editor added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,79 @@
+import { useCallback, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Result from "./components/Result";
+import ReactCodeMirror from "@uiw/react-codemirror";
+import { html } from "@codemirror/lang-html";
+import { css } from "@codemirror/lang-css";
+import { javascript } from "@codemirror/lang-javascript";
 
 function App() {
+  const [htmlEdit, setHtmlEdit] = useState("");
+  const [cssEdit, setCssEdit] = useState("");
+  const [jsEdit, setJsEdit] = useState("");
+
+  const htmlChange = useCallback((value) => {
+    setHtmlEdit(value);
+  }, []);
+
+  const cssChange = useCallback((value) => {
+    setCssEdit(value);
+  }, []);
+
+  const jsChange = useCallback((value) => {
+    setJsEdit(value);
+  }, []);
+
+  const srcCode = `<html>
+    <body>${htmlEdit}</body>
+    <style>${cssEdit}</style>
+    <script>${jsEdit}</script>
+  </html>`;
+
   return (
     <>
       <Navbar />
+      {/* main content  */}
+      <div className="p-2">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
+          <div className="bg-[#282c34] shadow p-4 rounded-lg">
+            <h2 className="text-lg text-white font-semibold mb-2">HTML</h2>
+            <ReactCodeMirror
+              className="text-xl border border-gray-700"
+              height="300px"
+              theme="dark"
+              value={htmlEdit}
+              extensions={[html(true)]}
+              onChange={htmlChange}
+            />
+          </div>
+          <div className="bg-[#282c34] shadow p-4 rounded-lg">
+            <h2 className="text-lg text-white font-semibold mb-2">CSS</h2>
+            <ReactCodeMirror
+              className="text-xl border border-gray-700"
+              height="300px"
+              theme="dark"
+              value={cssEdit}
+              extensions={[css(true)]}
+              onChange={cssChange}
+            />
+          </div>
+          <div className="bg-[#282c34] shadow p-4 rounded-lg">
+            <h2 className="text-lg text-white font-semibold mb-2">
+              JAVASCRIPT
+            </h2>
+            <ReactCodeMirror
+              className="text-xl border border-gray-700"
+              height="300px"
+              theme="dark"
+              value={jsEdit}
+              extensions={[javascript(true)]}
+              onChange={jsChange}
+            />
+          </div>
+        </div>
+        <Result srcCode={srcCode} />
+      </div>
     </>
   );
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,3 +1,18 @@
-export default function Result() {
-  return <div>Result</div>;
+/* eslint-disable react/prop-types */
+export default function Result({ srcCode }) {
+  return (
+    <div>
+      <div className="bg-[#282c34] p-4 shadow mt-4 rounded-lg">
+        <h2 className="text-lg text-white font-semibold mb-2">Result</h2>
+        <iframe
+          src=""
+          className="w-full h-60 border border-gray-700 rounded-md"
+          srcDoc={srcCode}
+          title="Output"
+          sandbox="allow-scripts"
+          width="100%"
+          height="100%"></iframe>
+      </div>
+    </div>
+  );
 }
